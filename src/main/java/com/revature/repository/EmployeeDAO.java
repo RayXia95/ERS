5d59b5f04b6b0d6cf0734a0874b441fd39592d36
Raymond <raymondxia95@gmail.com>
2018-03-13 13:32:50 -0400

    ERS employee DAO insert function implemented and works

--- src/main/java/com/revature/repository/EmployeeDAO.java
+++ src/main/java/com/revature/repository/EmployeeDAO.java
@@ -1,8 +1,11 @@
 package com.revature.repository;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Set;
+
+import org.apache.log4j.Logger;
 
 import com.revature.model.Employee;
 import com.revature.model.EmployeeToken;
@@ -10,17 +13,39 @@ import com.revature.util.ErsRepositoryUtil;
 
 public class EmployeeDAO implements EmployeeRepository
 {
+    private static final Logger logger = Logger.getLogger(EmployeeDAO.class);
 
     @Override
     public boolean insert(Employee employee)
     {
 	try (Connection connection = ErsRepositoryUtil.getErsRepositoryUtil().getConnection())
 	{
+	    logger.trace("Was able to get connection to insert");
+	    final String SQL = "INSERT INTO USER_T VALUES(NULL,?,?,?,?,?,?)";
+	    PreparedStatement statement = connection.prepareStatement(SQL);
+	    int parameterIndex = 0;
+
+	    statement.setString(++parameterIndex, employee.getFirstName());
+	    statement.setString(++parameterIndex, employee.getLastName());
+	    statement.setString(++parameterIndex, employee.getUsername());
+	    statement.setString(++parameterIndex, employee.getPassword());
+	    statement.setString(++parameterIndex, employee.getEmail());
+	    statement.setInt(++parameterIndex, employee.getEmployeeRole().getId());
+
+	    if ( statement.executeUpdate() != 0 )
+	    {
+		logger.trace("Was able to insert");
+		return true;
+	    }
+	    else
+	    {
+		return false;
+	    }
 
 	}
 	catch (SQLException e)
 	{
-
+	    logger.error("Unable register user, Probably unique id violated", e);
 	}
 	return false;
     }
@@ -79,6 +104,10 @@ public class EmployeeDAO implements EmployeeRepository
     {
 	// TODO Auto-generated method stub
 	return null;
+    }
+
+    public static void main(String[] args)
+    {
     }
 
 }
