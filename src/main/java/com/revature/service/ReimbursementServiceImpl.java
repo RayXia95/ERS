bab4a55981917ee24c216bfd31e9dddb87facc19
Raymond <raymondxia95@gmail.com>
2018-03-22 13:12:21 -0400

    Finished with the service layer

--- src/main/java/com/revature/service/ReimbursementServiceImpl.java
+++ src/main/java/com/revature/service/ReimbursementServiceImpl.java
@@ -5,64 +5,68 @@ import java.util.Set;
 import com.revature.model.Employee;
 import com.revature.model.Reimbursement;
 import com.revature.model.ReimbursementType;
+import com.revature.repository.ReimbursementDAO;
 
 public class ReimbursementServiceImpl implements ReimbursementService
 {
+    private static ReimbursementServiceImpl reimbursementService = new ReimbursementServiceImpl();
+
+    private ReimbursementServiceImpl()
+    {
+
+    }
+
+    public ReimbursementServiceImpl getReimbursementService()
+    {
+	return reimbursementService;
+    }
 
     @Override
     public boolean submitRequest(Reimbursement reimbursement)
     {
-	// TODO Auto-generated method stub
-	return false;
+	return ReimbursementDAO.getReimbursementDAO().insert(reimbursement);
     }
 
     @Override
     public boolean finalizeRequest(Reimbursement reimbursement)
     {
-	// TODO Auto-generated method stub
-	return false;
+	return ReimbursementDAO.getReimbursementDAO().update(reimbursement);
     }
 
     @Override
     public Reimbursement getSingleRequest(Reimbursement reimbursement)
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().select(reimbursement.getId());
     }
 
     @Override
     public Set<Reimbursement> getUserPendingRequests(Employee employee)
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().selectPending(employee.getId());
     }
 
     @Override
     public Set<Reimbursement> getUserFinalizedRequests(Employee employee)
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().selectFinalized(employee.getId());
     }
 
     @Override
     public Set<Reimbursement> getAllPendingRequests()
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().selectAllPending();
     }
 
     @Override
     public Set<Reimbursement> getAllResolvedRequests()
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().selectAllFinalized();
     }
 
     @Override
     public Set<ReimbursementType> getReimbursementTypes()
     {
-	// TODO Auto-generated method stub
-	return null;
+	return ReimbursementDAO.getReimbursementDAO().selectTypes();
     }
 
 }
