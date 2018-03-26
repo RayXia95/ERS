37c6e28f9b511305f8916e5e61e9ca15c9f92dcd
Raymond <raymondxia95@gmail.com>
2018-03-26 11:48:41 -0400

    Finished login controller

--- src/main/java/com/revature/controller/LoginControllerAlpha.java
+++ src/main/java/com/revature/controller/LoginControllerAlpha.java
@@ -2,16 +2,40 @@ package com.revature.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class LoginControllerAlpha implements LoginController {
+import com.revature.model.Employee;
+import com.revature.service.EmployeeServiceImpl;
 
-	@Override
-	public String login(HttpServletRequest request) {
-		return "login.html";
+public class LoginControllerAlpha implements LoginController
+{
+
+    @Override
+    public String login(HttpServletRequest request)
+    {
+	if ( request.getMethod().equals("GET") )
+	{
+	    return "login.html";
 	}
 
-	@Override
-	public String logout(HttpServletRequest request) {
-		return "login.html";
+	Employee credentials = new Employee();
+	credentials.setUsername(request.getParameter("username"));
+	credentials.setPassword(request.getParameter("password"));
+
+	Employee employee = EmployeeServiceImpl.getEmployeeServiceImpl().authenticate(credentials);
+
+	if ( employee == null )
+	{
+	    return "login.html";
 	}
+	request.getSession().setAttribute("loggedEmployee", employee);
+
+	return employee.toString();
+    }
+
+    @Override
+    public String logout(HttpServletRequest request)
+    {
+	request.getSession().invalidate();
+	return "login.html";
+    }
 
 }
